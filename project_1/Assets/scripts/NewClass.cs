6c9c55937c715cbe20e302b87156ab8bcff9a146
kay <kaitlynturner26@gmail.com>
2020-01-15 10:51:35 -0700

    variable lecture
    access mod + data type + name;

--- project_1/Assets/scripts/NewClass.cs
+++ project_1/Assets/scripts/NewClass.cs
@@ -7,9 +7,16 @@ public class NewClass : MonoBehaviour
     // Start is called before the first frame update
     
     public string NameString;
+    public int intNum;
+    public float floatNum;
+    public GameObject gameObj;
 
     private void Start()
     {
         print( NameString);
+        print(intNum);
+        print(floatNum);
+        print(gameObject);
+        
     }
 }
