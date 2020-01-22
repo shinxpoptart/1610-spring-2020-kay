7ae014f96ce0e635ad12ec67a7e028cd54d96746
kay <kaitlynturner26@gmail.com>
2020-01-22 10:50:28 -0700

    practice script
    practice print and lines

---
+++ project_1/Assets/scripts/PracticeScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PracticeScript : MonoBehaviour
+{ 
+    private int HealthValue;
+    public float HealthScore;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+   print(HealthValue);
+   print(HealthScore);
+   print("holy macaroni!!");
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
