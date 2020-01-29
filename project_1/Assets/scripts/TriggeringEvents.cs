c17b1f06dfe13c379781672e8a21fb3dc44dbf82
kay <kaitlynturner26@gmail.com>
2020-01-29 11:20:27 -0700

    mouse events
    with with on .... and what they do

---
+++ project_1/Assets/scripts/TriggeringEvents.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggeringEvents : MonoBehaviour
+{
+    public int Tester;
+    public float Floater;
+    public string String;
+    public UnityEvent enter;
+    public UnityEvent stay;
+    public UnityEvent exit;
+
+    public void OnTriggerEnter(Collider other)
+    {
+enter.Invoke();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        stay.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        exit.Invoke();
+    }
+}
+
