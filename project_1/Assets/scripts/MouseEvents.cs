79e8b5c69d80a53ad7446c75b76a21519cc290e4
kay <kaitlynturner26@gmail.com>
2020-02-05 11:17:21 -0700

    character controller
    learn how to move things

--- project_1/Assets/scripts/MouseEvents.cs
+++ project_1/Assets/scripts/MouseEvents.cs
@@ -1,18 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MouseEvents : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public UnityEvent mouseDownEvent;
+    public UnityEvent mouseDragEvent;
+    public UnityEvent mouseEnterEvent;
+    public UnityEvent mouseOverEvent;
+    public UnityEvent mouseExitEvent;
+    public UnityEvent mouseUpEvent;
+    public UnityEvent moveBlock;
+    private void OnMouseDown()
     {
-        
+    mouseDownEvent.Invoke();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDrag()
     {
-        
+      mouseDragEvent.Invoke();
+    }
+
+    private void OnMouseEnter()
+    {
+        mouseEnterEvent.Invoke();
+    }
+
+    private void OnMouseOver()
+    {
+        mouseOverEvent.Invoke();
+    }
+
+    private void OnMouseExit()
+    {
+        mouseExitEvent.Invoke();
+    }
+
+    private void OnMouseUp()
+    {
+      mouseUpEvent.Invoke();  
+    }
+
+    private void OnAnimatorMove()
+    {
+     moveBlock.Invoke();   
     }
 }
