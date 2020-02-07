d3c7b1977ccef487e3ebd24b96029a0481d5e8a6
kay <kaitlynturner26@gmail.com>
2020-02-07 16:10:29 -0700

    gravity, color, destroy
    add rigid body for gravity, change material color with if then and get key down, destroy game object with get key down.

---
+++ project_1/Assets/scripts/CharacterMover.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+    public Vector3 positionDirection;
+    public CharacterController controller;
+    public float speed = 10f;
+    public float gravity = 3f;
+    public float jumpForce = 20f;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        controller.Move(positionDirection*Time.deltaTime);
+        positionDirection.x = Input.GetAxis("Horizontal")*speed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            positionDirection.y = jumpForce;
+        }
+        
+        positionDirection.y -= gravity;
+
+    }
+}
