1fde6758f643d003e12b24f66830947bce54597e
kay <kaitlynturner26@gmail.com>
2020-02-12 16:41:48 -0700

    Spring connector

---
+++ project_1/Assets/scripts/springController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(SpringJoint))]
+public class springController : MonoBehaviour
+
+{
+    public SpringJoint springObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        springObj = GetComponent<SpringJoint>();
+        springObj.maxDistance = 1f;
+        springObj.minDistance = 1f;
+        springObj.anchor = Vector3.one;
+        springObj.tolerance = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
