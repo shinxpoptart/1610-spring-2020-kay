e57808d0dde1cdcff1d3eb7cee6b086e8e86d9de
kay <kaitlynturner26@gmail.com>
2020-02-19 11:24:26 -0700

    health and score

---
+++ project_1/Assets/scripts/ImageBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Image = UnityEngine.Experimental.UIElements.Image;
+
+[RequireComponent(typeof(Image))]
+
+public class ImageBehavior : MonoBehaviour
+{
+    private Image imageObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        imageObj = GetComponent<Image>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ project_1/Assets/scripts/floatData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class FloatData : ScriptableObject
+{
+
+    public float value = 1f;
+
+    public void Changevalue(float number)
+    {
+        value += number;
+    }
+
+}
