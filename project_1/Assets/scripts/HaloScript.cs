db37a55f08a57b4c6dae4aa7728377bc02f2d8c1
kay <kaitlynturner26@gmail.com>
2020-02-22 13:55:31 -0700

    trigger attempt
    no success in getting the capsul to trigger the player

---
+++ project_1/Assets/scripts/HaloScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(ParticleSystem))]
+
+public class HaloScript : MonoBehaviour
+
+{
+    public ParticleSystem particalObj;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        particalObj = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ project_1/Assets/scripts/KillerTriggerScript.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillerTriggerScript : MonoBehaviour
+{
+    //trying to figure out how to trigger a trail when player collides with killer
+    
+    public GameObject killerObj;
+    public GameObject playerObj;
+    public
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+       
+    }
+}
