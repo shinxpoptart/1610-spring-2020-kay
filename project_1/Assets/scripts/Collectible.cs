3cb4f07b87abb158c22cfc06342b86dccecbc936
kay <kaitlynturner26@gmail.com>
2020-03-11 11:13:36 -0600

    lists and loops

---
+++ project_1/Assets/scripts/Collectible.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class Collectible : ScriptableObject
+{
+    public int powerLevel = 10;
+    public Color collectibleColor = Color.red;
+    public Sprite collectibleSprite;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ project_1/Assets/scripts/ListAndLoops.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu]
+public class ListAndLoops : ScriptableObject
+{
+    
+    public List<string> stringList;
+    public string singleString;
+
+    public void AddToList(string stringObj)
+    {
+        stringList.Add(stringObj);
+    }
+
+    public void RemoveFromList(string stringObj)
+    {
+        for (int i = 0; i < stringList.Count; i++)
+        {
+            if (stringList[i] == stringObj)
+            {
+                stringList.Remove(stringObj);
+            }
+        }
+    }
+
+    public void SortList()
+    {
+        stringList.Sort();
+    }
+
+    public void CheckList()
+    {
+        foreach (var obj in stringList)
+        {
+            if (obj == singleString)
+            {
+                Debug.Log(obj);
+            }
+        }
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
