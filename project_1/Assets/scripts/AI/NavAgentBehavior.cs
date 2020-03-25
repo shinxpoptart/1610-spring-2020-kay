5ed14df1dc3d41e47b16483181ee2d9bb2e5cf0a
kay <kaitlynturner26@gmail.com>
2020-03-25 11:12:01 -0600

    AI

---
+++ project_1/Assets/scripts/AI/NavAgentBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class NavAgentBehavior : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    //public Vector3 location;
+    public Transform player;
+    public float speed = 8f;
+    
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = speed;
+    }
+    
+    void Update()
+    {
+        //agent.destination = location;
+        agent.destination = player.position;
+    }
+}
